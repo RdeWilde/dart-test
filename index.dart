f441136e51e8b15dac22d13c5b65c30ce0c7ac5a
Robert <email@robertdewilde.nl>
2016-05-17 18:39:54 +0200

    Backup

--- index.dart
+++ index.dart
@@ -7,7 +7,9 @@ import 'package:http2/transport.dart';
 import 'dart:convert';
 
 
-final ApiServer apiServer = new ApiServer(apiPrefix: '', prettyPrint: true);
+
+
+final ApiServer apiServer = new ApiServer(); //apiPrefix: '', prettyPrint: true
 
 main() async {
   Logger.root..level = Level.INFO..onRecord.listen(print);
@@ -24,81 +26,97 @@ main() async {
   final MultiProtocolHttpServer server = await MultiProtocolHttpServer.bind(InternetAddress.ANY_IP_V4, 8181, context);
 
 
-  String pathFromHeaders(List<Header> headers) {
-    for (int i = 0 ; i < headers.length; i++) {
-      if (ASCII.decode(headers[i].name) == ':path') {
-        return ASCII.decode(headers[i].value);
-      }
-    }
-    throw new Exception('Expected a :path header, but did not find one.');
-  };
-
 
   runZoned(() {
     server.startServing(
       apiServer.httpRequestHandler,
-      (ServerTransportStream stream) {
-        String path;
-        List<Header> headers;
-        List<int> data = new List();
-
-        stream.incomingMessages.listen(
-          (StreamMessage message) async {
-            print("Message");
-
-            if (message is HeadersStreamMessage) {
-              path = pathFromHeaders(message.headers);
-              if (path == null) throw 'no path given';
-
-              headers = message.headers;
-
-              //if (!path.startsWith(apiServer._apiPrefix)) {}
-              // if (stream.canPush())
-              //  stream.push(...);
-              //
-            } else if (message is DataStreamMessage) {
-              data.addAll(message.bytes);
-            };
-          },
-          onDone: () {
-            print("Done");
-
-            Map<String, dynamic> headerMap = new Map();
-            headers.forEach((header) {
-              headerMap[ASCII.decode(header.name)] = ASCII.decode(header.value);
-              print("${ASCII.decode(header.name)}: ${ASCII.decode(header.value)}");
-            });
-            HttpApiRequest request = new HttpApiRequest(
-                "GET",
-                Uri.parse(path),
-                headerMap,
-                new Stream.fromIterable(data)
-            );
-
-            apiServer.handleHttpApiRequest(request).then(
-              (HttpApiResponse response) async {
-                List<Header> headerList = new List();
-                response.headers.forEach((String key, dynamic value) {
-                  print("${key}: ${value}");
-                  headerList.add(
-                      new Header.ascii(key, value)
-                  );
-                });
-
-                return new Future(() {
-                  stream.sendHeaders(headerList);
-                  response.body.drain(stream.sendData);
-                  stream.outgoingMessages.close();
-                });
-              },
-              onError: (dynamic) {
-                print("Error");
-                stream.outgoingMessages.close();
-              }
-            );
-          }
-        );
-      }
+      apiServer.http2RequestHandler
+//      (ServerTransportStream stream) async {
+//        String path;
+//        List<Header> headers;
+//        List<int> data = new List();
+//
+//        stream.incomingMessages.listen(
+//          (StreamMessage message) async {
+//            print("Message");
+//
+//            if (message is HeadersStreamMessage) {
+//              path = pathFromHeaders(message.headers);
+//              if (path == null) throw 'no path given';
+//
+//              headers = message.headers;
+//
+//              //if (!path.startsWith(apiServer._apiPrefix)) {}
+//              // if (stream.canPush())
+//              //  stream.push(...);
+//              //
+//            } else if (message is DataStreamMessage) {
+//              data.addAll(message.bytes);
+//            };
+//          },
+//          onDone: () async {
+//            print("Done");
+//
+//            if (path == '/favicon.ico') {
+//              return new Future(() async {
+//                stream.outgoingMessages.add(
+//                    new HeadersStreamMessage([
+//                      new Header.ascii(':status', '204'),
+//                    ])
+//                );
+//                await stream.outgoingMessages.close();
+//              });
+//            } else {
+//              Map<String, dynamic> headerMap = new Map();
+//              headers.forEach((header) {
+//                headerMap[ASCII.decode(header.name)] =
+//                    ASCII.decode(header.value);
+//                print("${ASCII.decode(header.name)}: ${ASCII.decode(
+//                    header.value)}");
+//              });
+//
+//              HttpApiRequest request = new HttpApiRequest(
+//                  "GET",
+//                  Uri.parse(path),
+//                  headerMap,
+//                  new Stream.fromIterable(data)
+//              );
+//
+//              apiServer.handleHttpApiRequest(request).then(
+//                (HttpApiResponse response) {
+//                List<Header> headerList = new List();
+//                response.headers.forEach((String key, dynamic value) {
+//                  print("${key}: ${value}");
+//                  headerList.add(
+//                      new Header.ascii(key, value)
+//                  );
+//                });
+//
+//                return new Future(() async {
+//                  stream.outgoingMessages.add(
+//                      new HeadersStreamMessage([
+//                        new Header.ascii(':status', '200'),
+//                      ])
+//                  );
+//
+//                  await response.body.forEach((data) {
+//                    stream.outgoingMessages.add(
+//                        new DataStreamMessage(data)
+//                    );
+//                  });
+//
+//                  await stream.outgoingMessages.close();
+//                });
+//              },
+//                onError: (dynamic) {
+//                  print("Error");
+//                  stream.outgoingMessages.close();
+//                }
+//              );
+//            }
+//          }
+//        );
+//      }
     );
   }, onError: (e, s) {
     print("Unexpected error: $e");
